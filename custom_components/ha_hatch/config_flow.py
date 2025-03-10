f95c38172d5e5ca60102e553a2191818315e1688
Bren Dahl <dahl.brendan@gmail.com>
2025-03-09 22:18:06 -0400

    fix: prevent configuring the same account on this integration twice ...

--- custom_components/ha_hatch/config_flow.py
+++ custom_components/ha_hatch/config_flow.py
@@ -81,6 +81,8 @@ class ConfigFlowHandler(config_entries.ConfigFlow):
                 await api_cloud.login(email=email, password=password)
                 await self.async_set_unique_id(unique_id="hatch")
                 self.data.update(user_input)
+                await self.async_set_unique_id(email)
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=email,
                     data=self.data,
