a44d5e4e080011a04cf4716cbe4b7e9b804db4b5
Bren Dahl <dahl.brendan@gmail.com>
2025-04-07 20:37:10 -0400

    fix: retry updating auth in one minute when it fails (assumes api issue)

--- custom_components/ha_hatch/hatch_data_update_coordinator.py
+++ custom_components/ha_hatch/hatch_data_update_coordinator.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import timedelta, UTC, datetime
 from logging import getLogger, Logger
 from typing import List
 
 from hatch_rest_api import RestMini, RestPlus, RestIot, RestoreIot
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from custom_components.ha_hatch import DOMAIN
 
@@ -46,33 +46,38 @@ class HatchDataUpdateCoordinator(DataUpdateCoordinator[dict]):
             rest_device.remove_callback(self.async_update_listeners)
 
     async def _async_update_data(self) -> List[dict]:
-        _LOGGER.debug(f"_async_update_data: {self.email}")
-        self._disconnect_mqtt()
-        self._rest_device_unsub()
+        try:
+            _LOGGER.debug(f"_async_update_data: {self.email}")
+            self._disconnect_mqtt()
+            self._rest_device_unsub()
 
-        from hatch_rest_api import get_rest_devices
+            from hatch_rest_api import get_rest_devices
 
-        def disconnect():
-            _LOGGER.debug("disconnected")
+            def disconnect():
+                _LOGGER.debug("disconnected")
 
-        def resumed():
-            _LOGGER.debug("resumed")
+            def resumed():
+                _LOGGER.debug("resumed")
 
-        client_session = async_get_clientsession(self.hass)
-        _, self.mqtt_connection, self.rest_devices, self.expiration_time = await get_rest_devices(
-            email=self.email,
-            password=self.password,
-            client_session=client_session,
-            on_connection_interrupted=disconnect,
-            on_connection_resumed=resumed,
-        )
-        _LOGGER.debug(
-            f"credentials expire at: {datetime.datetime.fromtimestamp(self.expiration_time, datetime.UTC)}"
-        )
-        self.update_interval = datetime.datetime.fromtimestamp(self.expiration_time - 60, datetime.UTC) - datetime.datetime.now(datetime.UTC)
-        for rest_device in self.rest_devices:
-            rest_device.register_callback(self.async_update_listeners)
-        return [rest_device.__repr__() for rest_device in self.rest_devices]
+            client_session = async_get_clientsession(self.hass)
+            _, self.mqtt_connection, self.rest_devices, self.expiration_time = await get_rest_devices(
+                email=self.email,
+                password=self.password,
+                client_session=client_session,
+                on_connection_interrupted=disconnect,
+                on_connection_resumed=resumed,
+            )
+            _LOGGER.debug(
+                f"credentials expire at: {datetime.fromtimestamp(self.expiration_time, UTC)}"
+            )
+            self.update_interval = datetime.fromtimestamp(self.expiration_time - 60, UTC) - datetime.now(UTC)
+            for rest_device in self.rest_devices:
+                rest_device.register_callback(self.async_update_listeners)
+            return [rest_device.__repr__() for rest_device in self.rest_devices]
+        except Exception as error:
+            _LOGGER.exception(error)
+            self.update_interval = timedelta(minutes=1)
+            raise UpdateFailed(error) from error
 
     async def async_shutdown(self) -> None:
         """Cancel any scheduled call, and ignore new runs."""
