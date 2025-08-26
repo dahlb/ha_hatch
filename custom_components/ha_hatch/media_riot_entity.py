c9af279fe545bbb7815e9f98bc95a4d81d9cc484
Bren Dahl <dahl.brendan@gmail.com>
2025-08-26 10:11:16 -0400

    fix: birds playing unexpectedly

--- custom_components/ha_hatch/media_riot_entity.py
+++ custom_components/ha_hatch/media_riot_entity.py
@@ -22,6 +22,13 @@ from hatch_rest_api import REST_IOT_AUDIO_TRACKS
 _LOGGER = logging.getLogger(__name__)
 
 
+def _find_track(track_name) -> str | None:
+    return next(
+        (track for track in REST_IOT_AUDIO_TRACKS if track.name == track_name),
+        None,
+    )
+
+
 class MediaRiotEntity(HatchEntity, MediaPlayerEntity):
     _attr_media_content_type = MediaType.MUSIC
     _attr_device_class = MediaPlayerDeviceClass.SPEAKER
@@ -77,16 +84,8 @@ class MediaRiotEntity(HatchEntity, MediaPlayerEntity):
     def media_play(self) -> None:
         self.select_sound_mode(self._attr_sound_mode_list[0])
 
-    def _find_track(self, track_name) -> str | None:
-        if track_name is None:
-            track_name = self.sound_mode
-        return next(
-            (track for track in REST_IOT_AUDIO_TRACKS if track.name == track_name),
-            None,
-        )
-
     def select_sound_mode(self, sound_mode: str) -> None:
-        track = self._find_track(track_name=sound_mode)
+        track = _find_track(track_name=sound_mode)
         if track is None:
             self.rest_device.set_sound(sound_mode)
         else:
